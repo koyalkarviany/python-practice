b489101d1cdc06606370d1156841d1c1b8a5a440
koyalkar vinay <koyalkarvinaysuryavanshi@gmail.com>
2023-09-25 18:32:58 +0530

    python file added

---
+++ practice.py
@@ -0,0 +1,10 @@
+a = int(input('enter your number: '))
+print (a)
+b = int(input('enter you second number: '))
+print (b)
+# for i in range 
+if a is  =:
+       print('your entered correct number')
+    
+elif b== 40:
+    print('hoo your entered wrong number')
